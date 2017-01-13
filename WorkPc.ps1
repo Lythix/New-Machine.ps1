aca8365c020f0e8c6dc07a91b19c27b54ad79a1c
Stephen Price <stephen@lythixdesigns.com>
2017-01-13 13:41:01 +0800

    Changed to call choco

--- WorkPc.ps1
+++ WorkPc.ps1
@@ -11,11 +11,6 @@ if (-not $IsAdmin) {
 Write-Progress -Activity "Setting exeuction policy"
 Set-ExecutionPolicy RemoteSigned
 
-Write-Progress -Activity "Ensuring PS profile exists"
-if (-not (Test-Path $PROFILE)) {
-    New-Item $PROFILE -Force
-}
-
 Write-Progress -Activity "Ensuring Chocolatey is available"
 $null = Get-PackageProvider -Name chocolatey
 
@@ -58,39 +53,8 @@ if (-not ((Get-PackageSource -Name chocolatey).IsTrusted)) {
     
 ) | % {
     Write-Progress -Activity "Installing $_"
-    Install-Package -Name $_ -ProviderName chocolatey
+    Choco install $_ -y
 }
 
-Write-Progress -Activity "Setting git identity"
-$userName = (Get-WmiObject Win32_Process -Filter "Handle = $Pid").GetRelated("Win32_LogonSession").GetRelated("Win32_UserAccount").FullName
-Write-Verbose "Setting git user.name to $userName"
-git config --global user.name $userName
-# This seems to the be MSA that was first used during Windows setup
-$userEmail = "stephen@lythixdesigns.com"
-Write-Verbose "Setting git user.email to $userEmail"
-git config --global user.email $userEmail
 
-Write-Progress -Activity "Setting git push behaviour to squelch the 2.0 upgrade message"
-if ((& git config push.default) -eq $null) {
-    git config --global push.default simple
-}
 
-Write-Progress -Activity "Setting git aliases"
-git config --global alias.st "status"
-git config --global alias.co "checkout"
-git config --global alias.df "diff"
-git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-
-Write-Progress -Activity "Checking for Git Credential Manager"
-if ((& git config credential.helper) -ne "manager") {
-    Write-Warning "Git Credential Manager for Windows is missing. Install it manually from https://github.com/Microsoft/Git-Credential-Manager-for-Windows/releases"
-}
-
-Write-Progress "Enabling PowerShell on Win+X"
-if ((Get-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\).DontUsePowerShellOnWinX -ne 0) {
-    Set-ItemProperty HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\ -Name DontUsePowerShellOnWinX -Value 0
-    Get-Process explorer | Stop-Process
-}
-
-Write-Progress -Activity "Reloading PS profile"
-. $PROFILE
